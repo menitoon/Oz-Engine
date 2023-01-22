1c8ec4e4d20252fba8be4173d790c4306aecbc02
menitocode <120120351+menitoon@users.noreply.github.com>
2023-01-22 07:22:45 +0000

    --Oz Engine V1.5.5-- #Added : -Fully Cleaned up code -Canvas is now an object -Minor performance optimizations

--- Engine.py
+++ Engine.py
@@ -1,16 +1,8 @@
 import math
-
-sprite = []
-sprite_priority = []
-
-sprite_names = []
-sprite_names_dict = {}
-
-distances = []
-
-
+import time
 
 def deep_copy(L):
+
   if isinstance(L, list):
     ret = []
     for i in L:
@@ -23,229 +15,240 @@ def deep_copy(L):
   return ret
 
 
+class Canvas:
+
+  def __init__(self, SIZE: list, VOID):
+
+    self.VOID = VOID
+    self.SIZE = SIZE
+    self.SIZE_X = SIZE[0]
+    self.SIZE_Y = SIZE[1]
+
+    self.canvas = []
+    self.camera_pos = [0, 0]
+
+    self.CORNER_TOP_LEFT = (0, 0)
+    self.CORNER_TOP_RIGHT = (self.SIZE_X - 1, 0)
+    self.CORNER_BOTTOM_RIGHT = (self.SIZE_X - 1, self.SIZE_Y - 1)
+    self.CORNER_BOTTOM_LEFT = (0, self.SIZE_Y - 1)
+    self.MAX_DISTANCE = (
+      self.get_distance_between(self.CORNER_TOP_LEFT, self.CORNER_BOTTOM_LEFT)
+      + self.get_distance_between(self.CORNER_TOP_RIGHT,
+                                  self.CORNER_BOTTOM_LEFT) +
+      self.get_distance_between(self.CORNER_BOTTOM_RIGHT,
+                                self.CORNER_BOTTOM_LEFT) +
+      self.get_distance_between(self.CORNER_BOTTOM_LEFT,
+                                self.CORNER_BOTTOM_LEFT)) / 4
+
+    self.distances = []
+    self.sprite_tree = []
+    self.sprite_priority = []
+    self.sprite_names = []
+    self.sprite_names_dict = {}
+
+    self.create_canvas()
+
+
+
+  
+  def create_canvas(self):
+    # allow to define size of canvas
+
+    
+
+    SIZE = self.SIZE
+    SIZE_X = self.SIZE_X
+    SIZE_Y = self.SIZE_Y
+    VOID = self.VOID
+
+    if SIZE == [0, 0]:
+
+      print(
+        '\033[93m' +
+        "!Canvas size is not defined and will most likely not work.")  #warn
+      print("\u001b[0m")
+
+    x_line = []
+
+    for todo in range(SIZE_Y):
+
+      x_line.append(str(VOID))
+
+    for subtodo in range(SIZE_X):
+
+      self.canvas.append(x_line)
+
+
+  
+
+  def edit_element(self, x, y, char):
+
+    line = deep_copy(self.canvas[y])
+    line[x] = char
+    self.canvas[y] = line
+
+    return self.canvas
+
+  def get_canvas(self, is_string: bool = True):  #renders the canvas
+
+    
+    render_canvas = deep_copy(self.canvas) #deep_copy of empty canvas to stack sprite instance on it.
+    self.get_every_distance_from()         #define sprite_priority, which sprite should be rendered first
+    
+    
+
+    for current_sprite in self.sprite_priority:
+
+
+      current_pos = current_sprite.position #set position of the sprite we are looking at throught the for loop
+
+      camera_x = self.camera_pos[0]         #define x_axis of camera
+      camera_y = self.camera_pos[1]         #define y_axis of camera
+
+      current_pos_x = current_pos[0]        #define x_axis of the current sprite position we are looking at
+      current_pos_y = current_pos[1]        #define y_axis of the current sprite position we are looking at
+
+      RENDER_POS = [current_pos_x - camera_x, current_pos_y - camera_y]  #sprite postion - camera_position
+
+      RENDER_POS_X = RENDER_POS[0]          #define x_axis of render position
+      RENDER_POS_Y = RENDER_POS[1]          #define y_axis of render position
+
+      
+      current_distance = self.distances[self.sprite_priority.index(current_sprite)]  #gets the distance of sprite from square with sprite_priority index
+                                                                                     # sprite = [ sprite1 , sprite2 ]
+                                                                                     # distances = [4.0 , 5.0]
+                                                                                     # sprite_priority.index(current_sprite) returns 1 so = distances[1]
+
+      if current_distance > self.MAX_DISTANCE:
+        break
+
+      else:
+        render_canvas = self.edit_element(RENDER_POS_X, RENDER_POS_Y, current_sprite.char)
+
+      
+    if is_string:
+
+      line = ""
+
+      for current_line in render_canvas:
+
+        for current_element in current_line:
+          line += str(current_element)
+
+        line += "\n"
+      return line
+
+    else:
+      return render_canvas
+
+  def get_element(self, x, y):
+
+    line = self.canvas[y]
+    return line[x]
+
+  def get_distance_between(self, pos1, pos2):
+
+    return math.sqrt((pow((pos2[0] - pos1[0]), 2) + pow(
+      (pos2[1] - pos1[1]), 2)))  # √[(x₂ - x₁)² + (y₂ - y₁)²]
+
+  def get_every_distance_from(self):
+
+    self.distances = []
+
+    camera_pos = self.camera_pos
+    CORNER_TOP_LEFT = self.CORNER_TOP_LEFT
+    CORNER_TOP_RIGHT = self.CORNER_TOP_RIGHT
+    CORNER_BOTTOM_RIGHT = self.CORNER_BOTTOM_RIGHT
+    CORNER_BOTTOM_LEFT = self.CORNER_BOTTOM_LEFT
+
+    camera_pos_x = camera_pos[0]
+    camera_pos_y = camera_pos[1]
+
+    for todo_sprite in self.sprite_tree:
+
+      sprite_pos = [
+        todo_sprite.position[0] - camera_pos_x,
+        todo_sprite.position[1] - camera_pos_y
+      ]
+
+      distance_calculated = (
+        self.get_distance_between(CORNER_TOP_LEFT, sprite_pos) +
+        self.get_distance_between(CORNER_TOP_RIGHT, sprite_pos) +
+        self.get_distance_between(CORNER_BOTTOM_RIGHT, sprite_pos) +
+        self.get_distance_between(CORNER_BOTTOM_LEFT, sprite_pos)) / 4
+
+      self.distances.append(distance_calculated)
+
+    self.sprite_priority = self.sprite_tree
+    sorted_list = []
+    new_sprite_priority = []
+
+    for todo in range(len(self.distances)):
+
+      min_distance = min(self.distances)
+
+      sorted_list.append(min_distance)
+
+      new_sprite_priority.append(self.sprite_priority[self.distances.index(
+        min_distance)])  #gets the correct sprite associated to it 's distance
+
+      self.sprite_priority.remove(
+        self.sprite_priority[self.distances.index(min_distance)])
+      self.distances.remove(min(self.distances))
+
+    self.sprite_priority = new_sprite_priority
+    self.sprite_tree = self.sprite_priority
+    self.distances = sorted_list
+
+  def send_light_update(self):
+
+    x = 0
+    y = 0
+
+    for line in self.get_canvas(False):
+
+      for element in line:
+
+        #display.set_pixel(x, y, element)
+        x += 1
+
+      x = 0
+      y += 1
+
+
 class Sprite:
 
-  def __init__(self, char : str, position : list, name : str , group =None  ):
+  #sprite must start with "s" like this when initiated :   S_name_of_sprite
+
+  def __init__(self,
+               canvas_object: object,
+               char: str,
+               position: list,
+               name: str,
+               group=None):
 
     self.char = char
     self.position = position
     self.name = name
+    self.canvas_object = canvas_object
 
-    sprite.append(self)
+    canvas_object.sprite_tree.append(self)
 
-    if name not in sprite_names:
-    
-      sprite_names.append(self.name)
-      sprite_names_dict[self.name] = self
+    if name not in canvas_object.sprite_names:
+
+      canvas_object.sprite_names.append(self.name)
+      canvas_object.sprite_names_dict[self.name] = self
 
     else:
-
-      raise(ValueError("Sprite name already exists , please choose another name , only unique names are allowed or consider deleting the older one."))
-      
+      #crash and send the follow error back:
+      raise (ValueError(
+        "Sprite name already exists , please choose another name , only unique names are allowed or consider deleting the older one."
+      ))
 
   def destroy(self):
 
-    sprite.remove(self)
+    self.canvas_object.sprite_tree.remove(self)
     del self
-
-  #sprite must start with "S" like this :   S_name_of_sprite
-
-
-canvas = []
-camera_pos = [0, 0]
-size = [0, 0]
-
-sprite_priority = []
-
-
-def _create_canvas(void):
-  # allow to define size of canvas
-
-  x = size[0]
-  y = size[1]
-
-  if size == [0 , 0]:
-    
-    print('\033[93m' + "!Canvas size is not defined and will most likely not work.") #warn
-  
-  x_line = []
-
-  for todo in range(x):
-
-    x_line.append(str(void))
-
-  for subtodo in range(y):
-
-    canvas.append(x_line)
-
-
-
-def _edit_element(x, y, char, name_canvas):
-
-  line = deep_copy(name_canvas[y])
-  line[x] = char
-  name_canvas[y] = line
-
-  return name_canvas
-
-
-def _get_canvas(is_string: bool):  #renders the canvas
-
-  line = ""
-
-  render_canvas = deep_copy(canvas)
-
-  corner_one = [0, 0]
-  corner_two = [size[0] - 1, 0]
-  corner_three = [size[0] - 1, size[1] - 1]
-  corner_four = [0, size[1] - 1]
-
-  pos = [size[0] - 1, size[1] - 1]
-
-  max_distance = (_get_distance_between(corner_one, pos) +
-                  _get_distance_between(corner_two, pos) +
-                  _get_distance_between(corner_three, pos) +
-                  _get_distance_between(corner_four, pos)) / 4
-
-  _get_every_distance_from()
-
-  todo = 0
-
-
-
-  for current_sprite in sprite_priority:
-
-    
-
-    current_pos = current_sprite.position
-
-    render_check_pos = [
-      current_pos[0] - camera_pos[0], current_pos[1] - camera_pos[1]
-    ]
-    
-    current_pos = deep_copy(render_check_pos)
-
-    
-
-    # check if can render
-
-    print( distances[sprite_priority.index(current_sprite)] , max_distance , current_sprite.name)
-
-    if distances[sprite_priority.index(current_sprite)] > max_distance:
-
-      
-      
-      break
-
-    else:
-      
-      print([current_pos[0] , current_pos[1] ])
-      _edit_element(current_pos[0], current_pos[1], current_sprite.char,
-                    render_canvas)
-
-    todo += 1
-
-  if is_string:
-
-    for current_line in render_canvas:
-
-      for current_element in current_line:
-
-        line += str(current_element)
-
-      line += "\n"
-
-    return line
-
-  else:
-
-    return render_canvas
-
-
-def _get_element(x, y):
-
-  line = canvas[y]
-  return line[x]
-
-
-def _get_distance_between(pos1, pos2):
-
-  return math.sqrt((pow((pos2[0] - pos1[0]), 2) + pow(
-    (pos2[1] - pos1[1]), 2)))  # √[(x₂ - x₁)² + (y₂ - y₁)²]
-
-
-def _get_every_distance_from():
-
-  global distance_from
-  global distances
-  global sprite_priority
-  global sprite
-  
-  distances = []
-  
-
-  corner_one = [0, 0]
-  corner_two = [size[0] - 1, 0]
-  corner_three = [size[0] - 1, size[1] - 1]
-  corner_four = [0, size[1] - 1]
-
-  
-
-  for todo_sprite in sprite:
-
-    gloabal_pos = [
-      todo_sprite.position[0] - camera_pos[0],
-      todo_sprite.position[1] - camera_pos[1]
-    ]
-
-    distance_calculated = (_get_distance_between(corner_one, gloabal_pos) +
-                           _get_distance_between(corner_two, gloabal_pos) +
-                           _get_distance_between(corner_three, gloabal_pos) +
-                           _get_distance_between(corner_four, gloabal_pos)) / 4
-
-
-    distances.append(distance_calculated)
-
-
-  
-  sprite_priority = sprite
-  sorted_list = []
-  new_sprite_priority = []
-
-  while distances != [] :
-
-    min_distance = min(distances)
-    
-    sorted_list.append(min_distance)
-    
-    new_sprite_priority.append( sprite_priority[distances.index(min_distance)] ) #gets the correct sprite associated to it 's distance
-    
-    sprite_priority.remove(sprite_priority[distances.index(min_distance)])
-    distances.remove(min(distances))
-    
-  
-  sprite_priority = new_sprite_priority
-  sprite = sprite_priority
-  distances = sorted_list
-  
-  
-
-
-def _send_light_update():
-
-  x = 0
-  y = 0
-
-  for line in _get_canvas(False):
-
-    for element in line:
-
-      #display.set_pixel(x, y, element)
-      x += 1
-
-    x = 0
-    y += 1
-
 
 
 
