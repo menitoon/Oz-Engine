5a29cafa65cd6afd111a7ae0216a7860bcd802b1
Menit <120120351+menitoon@users.noreply.github.com>
2023-06-10 11:49:19 +0200

    Fixed Rows based-Render Issues

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='OzEngine',
-    version='1.3.0',
+    name='Oz-Engine',
+    version='1.3.0.1',
     description='A library for making graphic text-based games in the console',
-    packages=['OzEngine'],
+    packages=['Oz-Engine'],
     install_requires=[],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
